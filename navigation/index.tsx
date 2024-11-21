a82482fba0ff0a69f23924a71c5bc62cb920ae10
Mani Rastegari <mani.rastegari@gmail.com>
2024-11-21 18:57:28 -0500

    BeforeRootNav2

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,1 +1,11 @@
-import React from "react";
+
+import React from 'react';
+import { userAuthentication } from '../config/userAuthentication';
+import HomeStack from './HomeStack';
+import WelcomeStack from './WelcomeStack';
+
+export default function RootNavigation() {
+    const { user } = userAuthentication();
+
+    return user ? <HomeStack /> : <WelcomeStack />
+}
